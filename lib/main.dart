c9ef8e41c613b177f1f34a28408b14b51a1abb35
PhAlves23 <ph23.alves@gmail.com>
2021-10-30 16:17:39 -0300

    criando interface

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:caraoucoroa/tela_inicial.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: TelaInicial(),
+  ));
+}
---
+++ lib/tela_inicial.dart
@@ -0,0 +1,13 @@
+class TelaInicial extends StatefulWidget {
+  const TelaInicial({Key? key}) : super(key: key);
+
+  @override
+  _TelaInicialState createState() => _TelaInicialState();
+}
+
+class _TelaInicialState extends State<TelaInicial> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
